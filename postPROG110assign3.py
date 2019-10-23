922e5e5d371382ecfeee5892d4d544aa1304eca0
jamespost <32137126+jamespost@users.noreply.github.com>
2019-10-23 09:25:26 -0500

    added defualt string function

--- postPROG110assign3.py
+++ postPROG110assign3.py
@@ -1,12 +1,23 @@
 """James Post PROG 110 Assignment 3"""
-class AudioEffects():#define a class name "AudioEffects"
+class AudioEffects():#define a class name "AudioEffects" using psuedocode
     def __init__(self, filename):
         """Initialize effect attributes"""
+        #store the filename of the audiofile being passed in
+        self.filename = filename
+        #calculate bit-depth
+        self.bitDepth = int(16)#default bit depth is 16bit
+        #calculate sample-rate
+        self.sampleRate = int(44100)#default sample rate is 44100
         #calculate length of audio file
         self.length = int(44100) #default file length is 44100 samples (1 second)
         #calculate amplitude peaks
-        self.amplitudePeaks = float(0)#default peak is 0dB
-        #calculate sample-rate
-        self.sampleRate = int(44100)#default sample rate is 44100
-        #calculate bit-depth
-        self.bitDepth = int(16)#default bit depth is 16bit
+        self.amplitudePeaks = float(0)#default peak is 0dB        
+
+    def __str___(self):
+            """Defines what to do when the default python str() function is called on an instance"""
+            print("Audio File Info:")
+            print("\tFilename = " , self.filename)
+            print("\tBit Depth = " , self.bitDepth)
+            print("\tSample Rate = " , self.sampleRate)
+            print("\tLength = " , self.length)
+            print("\tAmplitude Peaks = " , self.amplitudePeaks)
